aa3460bc2c608cb8255796dce39a3338c9b1466c
= <=>
2020-10-07 19:43:43 +0700

    Tao project theo mo hinh MVC phan 2

--- news-jdbc-2020/src/main/java/com/william/controller/admin/HomeController.java
+++ news-jdbc-2020/src/main/java/com/william/controller/admin/HomeController.java
@@ -1,15 +1,13 @@
-package com.william.controller.web;
+package com.william.controller.admin;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/trang-chu"})
+@WebServlet(urlPatterns = {"/admin-home"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -18,8 +16,7 @@ public class HomeController extends HttpServlet {
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
-		rd.forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
