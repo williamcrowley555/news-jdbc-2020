32e34754c23c01c1e67c58cb70c63612625f6a56
= <=>
2020-10-11 23:47:36 +0700

    Xay dung giao dien bootstrap phan 3

--- news-jdbc-2020/src/main/java/com/william/controller/admin/HomeController.java
+++ news-jdbc-2020/src/main/java/com/william/controller/admin/HomeController.java
@@ -1,6 +1,8 @@
 package com.william.controller.admin;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +18,8 @@ public class HomeController extends HttpServlet {
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/views/admin/home.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
