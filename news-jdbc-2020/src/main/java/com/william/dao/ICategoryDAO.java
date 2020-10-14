0dfb6bd74a91889af7a7fef0c1b756fea2d297e1
= <=>
2020-10-14 22:16:50 +0700

    Add mysql jdbc driver maven in pom.xml

---
+++ news-jdbc-2020/src/main/java/com/william/dao/ICategoryDAO.java
@@ -0,0 +1,9 @@
+package com.william.dao;
+
+import java.util.List;
+
+import com.william.model.CategoryModel;
+
+public interface ICategoryDAO {
+	List<CategoryModel> findAdll();
+}
---
+++ news-jdbc-2020/src/main/java/com/william/dao/impl/CategoryDAO.java
@@ -0,0 +1,16 @@
+package com.william.dao.impl;
+
+import java.util.List;
+
+import com.william.dao.ICategoryDAO;
+import com.william.model.CategoryModel;
+
+public class CategoryDAO implements ICategoryDAO {
+
+	@Override
+	public List<CategoryModel> findAdll() {
+		
+		return null;
+	}
+
+}
